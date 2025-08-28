b2f6763b6e719d7fcb8e13b8f6e9dbac9edb97dc
Dmitry <demosne@gmail.com>
2025-08-28 11:15:39 +0300

    Refactor ReservationService to include error handling for invalid IDs and conflicts; update methods to return actual data from the database instead of placeholder strings.

--- src/modules/reservation/reservation.service.ts
+++ src/modules/reservation/reservation.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+} from '@nestjs/common';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 import { UpdateReservationDto } from './dto/update-reservation.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -12,18 +16,43 @@ export class ReservationService {
   }
 
   findAll() {
-    return `This action returns all reservation`;
+    return this.prisma.reservation.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} reservation`;
+    if (!id) {
+      throw new BadRequestException('ID is required');
+    }
+    return this.prisma.reservation.findUnique({ where: { id } });
   }
 
-  update(id: number, updateReservationDto: UpdateReservationDto) {
-    return `This action updates a #${id} reservation`;
+  async update(id: number, updateReservationDto: UpdateReservationDto) {
+    if (!id) {
+      throw new BadRequestException('ID is required');
+    }
+
+    const targetResarvation = await this.findOne(id);
+    if (!targetResarvation) {
+      throw new ConflictException(`There is no reservation with ID: ${id}`);
+    }
+
+    const data: any = {};
+    if (updateReservationDto.user_id)
+      data.user_id = updateReservationDto.user_id;
+    if (updateReservationDto.parking_spot_number)
+      data.parking_spot_number = updateReservationDto.parking_spot_number;
+    if (updateReservationDto.reserved_date)
+      data.reserved_date = updateReservationDto.reserved_date;
+    if (updateReservationDto.reserved_time)
+      data.reserved_time = updateReservationDto.reserved_time;
+
+    return this.prisma.reservation.update({ where: { id }, data });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} reservation`;
+    if (!id) {
+      throw new BadRequestException('ID is required');
+    }
+    return this.prisma.reservation.delete({ where: { id } });
   }
 }
